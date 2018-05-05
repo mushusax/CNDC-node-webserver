46a7bfdeb398b5db52a6bbf5a9c169a90b3e94f9
Anshu <mushu.aradhyula@gmail.com>
2018-05-04 20:56:09 -0500

    not tracking package-lock.json anymore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -53,6 +54,6 @@ app.get('/bad', (request, response) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000');
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
 });
